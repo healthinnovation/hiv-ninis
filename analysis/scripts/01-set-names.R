438d377f35a317ac58afa4d8cb18e79cc8affc68
Diego Villa <ddevildiego@gmail.com>
2021-12-01 18:49:53 -0500

    Set variable values

--- analysis/scripts/01-set-names.R
+++ analysis/scripts/01-set-names.R
@@ -15,7 +15,9 @@ variable_names <- read_csv(path_variable_names_file)
 path_interim_data_dir <- path("data", "interim")
 dataset_wo_names_file <- "dataset-without-names.csv"
 path_dataset_wo_names_file <- path(path_interim_data_dir, dataset_wo_names_file)
-dataset_wo_names <- read_csv(path_dataset_wo_names_file)
+dataset_wo_names <- read_csv(
+  path_dataset_wo_names_file, col_types = cols(.default = "c")
+)
 
 # Set variable names ------------------------------------------------------
 
@@ -37,28 +39,34 @@ variables_to_filter_for_pivot <- c(
   "total_children_ever_born", "sons_who_have_died", "daughters_who_have_died",
   "births_last_five_years", "births_past_year", "births_month_interview", 
   "date_first_birth", "age_respondent_first_birth", "duration_current_pregnancy",
-  "number_living_children", "living_children_current_pregnancy", 
-  "marriage_fisrt_birth_interval", "last_birth_interview_interval",
-  "length_time_knows_last_partner"
+  "no_living_children", "living_children_current_pregnancy", 
+  "marriage_fisrt_birth_interval", "last_birth_interview_interval", "age", 
+  "no_household_members", "no_children_6_yo", 
+  "no_eligible_women_15_to_49_yo", "age_household_head", "age_household_member",
+  "mother_line_no", "father_line_no", "single_years_edu_partner",
+  "no_sex_partners_other_partner_last_12_months", 
+  "no_sex_partners_including_partner_last_12_months",
+  "years_lived_place_residence", "age_first_sexual_partner",
+  "total_lifetime_sex_partners_last_12_months", "highest_year_edu_woman",
+  "time_get_primary_source_drinking_water", "highest_year_edu", 
+  "edu_single_years", "grade_edu_current_year", 
+  "edu_single_years_current_year", "grade_edu_previous_year",
+  "edu_single_years_previous_year", "length_time_knows_last_partner"
 )
 
-values_per_variable_no_filter <- 
-  dataset %>%
-  # select() %>%
+dataset %>%
+  select(-all_of(variables_to_filter_for_pivot)) %>% 
   pivot_longer(
-    cols = !any_of(variables_to_filter_for_pivot),
+    cols = everything(),
     names_to = "variable_name",
     values_to = "value"
   ) %>%
   group_by(variable_name) %>%
-  summarise(values = sort(unique(value)), .groups = "drop") %>%
+  summarise(value = sort(unique(value)), .groups = "drop") %>%
   left_join(variable_names, by = "variable_name") %>%
-  select(survey, variable, variable_name, values) %>%
-  arrange(survey, variable, values) 
-
-values_per_variable <- 
-  values_per_variable_no_filter %>% 
-  filter
+  select(survey, variable, variable_name, value) %>%
+  arrange(survey, variable, value) ->
+  values_per_variable
 
 # Read variable values ----------------------------------------------------
 
@@ -66,32 +74,26 @@ variable_values_file <- "variable-values.csv"
 path_variable_values_file <- path(path_variables_dir, variable_values_file)
 variable_values <- read_csv(path_variable_values_file)
 
-# Set variable values -----------------------------------------------------
+# Filter variables without value description ------------------------------
 
-variable_values_path <- path(variables_path, "variable-values.csv")
-variable_values <- read_csv(variable_values_path)
+values_per_variable %>% 
+  anti_join(
+    variable_values, by = c("survey", "variable", "variable_name", "value")
+  ) ->
+  variable_values_to_fill
 
-for (i in 1:nrow(variable_values)) {
-  variable_name <- variable_values$variable_name[i]
-  value <- as.character(variable_values$value[i])
-  value_description <- variable_values$value_description[i]
-  
-  dataset[variable_name][dataset[variable_name] == value] <- value_description
-}
+variable_values_to_fill$value_description <- rep(NA, nrow(variable_values_to_fill))
 
-# is_numeric function------------------------------------------------------
+# Export variable values to fill ------------------------------------------
 
-# is_numeric <- function(x) {
-#   if (sum(grepl("^[0a-zA-Z].*", x)) > 0) {
-#     FALSE
-#   } else {
-#     TRUE
-#   }
-# }
+variables_out_path <- path("data", "variables", "raw")
+output_file_name <- "variable-values-to-fill.csv"
+output_file_path <- path(variables_out_path, output_file_name)
+write_csv(variable_values_to_fill, output_file_path, na = "")
 
+# Export data set ---------------------------------------------------------
 
-# Filter and parsing (numeric) variables ----------------------------------
-
-# Export wrangled dataset
-
-write.csv(dataset, "data/processed/hiv-ninis-dataset.csv", row.names = FALSE)
+data_out_path <- path("data", "interim")
+data_out_file_name <- "dataset-without-values.csv"
+data_out_file_path <- path(data_out_path, data_out_file_name)
+write_csv(dataset, data_out_file_path, na = "")
